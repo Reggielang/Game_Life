bb16bb4449068b2140c6b29e812359f72292bc3c
Honglang luo <819343713@qq.com>
2021-03-05 10:31:19 +0800

    10天

---
+++ Game_Test/碰撞方法/Assets/Scripts/move.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+    private Rigidbody rigi;
+    public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+        rigi = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigi.MovePosition(transform.position + Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigi.MovePosition(transform.position + Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigi.MovePosition(transform.position + Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigi.MovePosition(transform.position + Vector3.right * speed * Time.deltaTime);
+        }
+    }
+    void OnCollisionEnter(Collision coll)
+    {
+        print(coll.gameObject.name);
+    }
+    void OnCollisionStay(Collision coll)
+    {
+        print(coll.gameObject.name);
+    }
+
+    void OnCollisionExit(Collision coll)
+    {
+        print("离开"+coll.gameObject.name);
+    }
+
+}
